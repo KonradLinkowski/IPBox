a8ad6ae2328cdbff5b7511be62d06cd934280e36
Konrad Linkowski <linkowski.konrad97@gmail.com>
2020-10-05 18:04:54 +0200

    Improve unit tests :)

--- test/test.js
+++ test/test.js
@@ -5,10 +5,10 @@ const assert = require('assert');
 
 const ipInfoTestData = TestUtil.getIPInfoTestData();
 
-describe('IP Info', function () {
+describe('IP Info', () => {
 
-    describe('getIPInfo', function () {
-        it('should fetch info about specific IP', async function () {
+    describe('getIPInfo', () => {
+        it('should fetch info about specific IP', async () => {
             const ip = "186.12.170.197";
             const result = await getIPInfo(ip)
 
@@ -16,13 +16,13 @@ describe('IP Info', function () {
         });
     });
 
-    describe('addEntry', function () {
-        it('should add new entry to database', async function () {
+    describe('addEntry', () => {
+        it('should add new entry to database', async () => {
             const result = await database.addEntry(ipInfoTestData);
             assert.strictEqual(result.ip, ipInfoTestData.ip ,"IP Info saved");
         })
 
-        it('should throw duplicate key exception', async function () {
+        it('should throw duplicate key exception', async () => {
             await assert.rejects(async () => {
                 await database.addEntry(ipInfoTestData)
             }, {code: 11000}, "Entry wasn't rejected for duplicate key");
